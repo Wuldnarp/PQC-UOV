308665156a1dd1659e0c1db4dcd0b8196bb9adaa
JakobKjellberg02 <jakob.zornigkjellberg@gmail.com>
2026-06-11 15:35:44 +0200

    Boilerplate code for hashing
    Crates added to help with AES and SHAKE256. Functions are very simple
    and are just boilerplate for now. Added some schizo comments about how
    it should function with our custom data structures, but will take a
    closer look when field.rs and matrix.rs are implemented. Will also take
    a closer look at the paper and c code for cool tricks.

--- src/hash.rs
+++ src/hash.rs
@@ -1,14 +1,32 @@
+use crate::field::F16Element;
 use crate::matrix::FieldMatrix;
 use crate::matrix::FieldVector;
+use ctr::cipher::KeyIvInit;
+use ctr::cipher::StreamCipher;
+use shake::Shake256;
+use shake::XofReader;
+use shake::digest::{Update, ExtendableOutput};
+
+pub type Aes128Ctr = ctr::Ctr128BE<aes::Aes128>;
 
 /// Hash the message with salt to produce the target vector t ∈ F_16^m
 /// 
 /// t is what the signature must map to under the public polynomials
 /// 
-/// Implemented as SHAKE256(message || salt)
+/// Implemen_reader SHAKE256(message || salt)
 /// 
 /// output is m F_16 elements
+/// 
 fn hash(message: &[u8], salt: &[u8], m: usize) -> FieldVector{
+    let mut hasher = Shake256::default();
+    hasher.update(message);
+    hasher.update(salt);
+
+    let mut reader = hasher.finalize_xof();
+    // let elements = need some kind of unpacking
+    //  For F16 , we pack two field elements into one byte with the first element in the least
+    // significant nibble.
+    // FieldVector(elements)
     todo!()
 }
 
@@ -20,15 +38,38 @@ fn hash(message: &[u8], salt: &[u8], m: usize) -> FieldVector{
 /// 
 /// output is (n-m)*m F_16 elements
 fn expand_sk(seed: &[u8]) -> FieldMatrix{
+    let mut hasher = Shake256::default();
+    hasher.update(seed);
+    let mut reader = hasher.finalize_xof();
     todo!()
 }
 
-/// Expand the public seed into the random matrices P¹ and P²
-/// 
-/// P¹ has shape (n-m) × (n-m), P² has shape (n-m) × m
-/// 
-/// Implemented as AES128-CTR(seed_pk)
-fn expand_pk(seed: &[u8]) -> (Vec<FieldMatrix>, Vec<FieldMatrix>){
+fn expand_p(seed_pk: &[u8]) -> (Vec<FieldMatrix>, Vec<FieldMatrix>) {
+    // Maybe a better place to have parameters
+    const M: usize = 64;  
+    // n - m
+    const V: usize = 96;
+
+    // maybe the math aint mathing but will look at it
+    // m * ((n-m)(n-m+1))/2 and sub (n+m) for V
+    let p1_elements_per_matrix = (V * (V + 1)) / 2;
+    let total_p1_elements = p1_elements_per_matrix * M;
+    // (n-m) * m
+    let total_p2_elements = (V * M) * M;
+    let total_elements = total_p1_elements + total_p2_elements;
+    // For F16​, we pack two field elements into one byte with the first element in the least significant nibble.
+    let total_bytes = (total_elements + 1) / 2;
+
+    let key: [u8; 16] = seed_pk.try_into().expect("seed_pk must be exactly 16 bytes");
+    let iv = [0u8; 16];
+    // Doesn't seem like the crate offers round-reduction to 4
+    let mut cipher = Aes128Ctr::new(&key.into(), &iv.into());
+    let mut keystream = vec![0u8; total_bytes];
+    cipher.apply_keystream(&mut keystream);
+
+    //Need a way to unpack bytes into F16Element instances
+    // And split segments to populate matrices
+
     todo!()
 }
 
@@ -38,5 +79,12 @@ fn expand_pk(seed: &[u8]) -> (Vec<FieldMatrix>, Vec<FieldMatrix>){
 /// 
 /// Implemented as SHAKE256(message || salt || seed_sk || ctr)
 fn expand_v(message: &[u8], salt: &[u8], seed: &[u8], ctr: u8) -> FieldVector{
+    // maybe
+    let mut hasher = Shake256::default();
+    hasher.update(message);
+    hasher.update(salt);
+    hasher.update(seed);
+    hasher.update(&[ctr]);
+    let mut reader = hasher.finalize_xof();
     todo!()
 }
